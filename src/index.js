42b2581046bb32b4c0c905df0aab4267f4e6d515
Artem Khvostyk <5hv5art@gmail.com>
2021-08-28 17:04:30 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const openCloseBraces = bracketsConfig.reduce(
+    (prevVal, [openBrace, closeBrace]) => ({
+      ...prevVal,
+      [openBrace]: closeBrace,
+    }),
+    {}
+  );
+
+  const temp = [];
+
+  for (let i = 0; i < str.length; i++) {
+    const brace = str[i];
+    const lastBrace = temp[temp.length - 1];
+
+    if (brace === openCloseBraces[brace] && brace === lastBrace) {
+      temp.pop();
+    } else if (openCloseBraces.hasOwnProperty(brace) || !lastBrace) {
+      temp.push(brace);
+    } else if (brace === openCloseBraces[lastBrace]) {
+      temp.pop();
+    } else {
+      break;
+    }
+  }
+
+  return temp.length === 0;
+};
